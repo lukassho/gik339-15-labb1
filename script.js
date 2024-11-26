aee136b7534e9c21c0fc47b9498b422ef0739201
lukassho <110125953+lukassho@users.noreply.github.com>
2024-11-26 15:34:18 +0100

    Card-1 skapad

---
+++ script.js
@@ -0,0 +1,13 @@
+const cl = console.log;
+
+const checkbox = document.getElementById("checkbox");
+
+const text = document.getElementsByClassName("textfield");
+
+const btn = document.querySelector("button");
+
+const div = document.getElementsByTagName("div");
+
+function listener() {
+    cl(target);
+}
