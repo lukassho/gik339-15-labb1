4acbe80befe7df74e86b26ee2f7919eb144bcf76
lukassho <110125953+lukassho@users.noreply.github.com>
2024-11-29 14:47:34 +0100

    class-namn ändrade

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const cl = console.log;
-
 const checkbox = document.getElementById("checkbox");
-
-const text = document.getElementsByClassName("textfield");
-
+const input = document.querySelectorAll("input");
 const btn = document.querySelector("button");
+const output = document.getElementsByTagName("output");
 
-const div = document.getElementsByTagName("div");
-
-for(let i = 0; i < text.length; i++){
-    text[i].addEventListener('click', inputFiledListener);
+for(let i = 0; i < input.length; i++){
+    input[i].addEventListener('click', inputFiledListener);
 }
 
 function inputFiledListener(e) {
     const targetName = e.target.id;
     console.log('textfältet', targetName, 'klickades.');
+}
+
+input.forEach((field) => {
+    field.addEventListener('blur', handleBlur);
+});
+
+
+function handleBlur(e) {
+   const name = e.target.name;
+   const value = e.target.value;
+   const outputDiv = document.querySelector('.output');
+   outputDiv.innerHTML = value;
+   console.log(value);
 }
